db33885d103090ea54011229d9b7732ab55029c5
ilobna <lobna.elkerdawy@yahoo.com>
2024-02-27 14:17:53 +0000

    Added Javascript and updated HTML

--- world-clock/javascript/index.js
+++ world-clock/javascript/index.js
@@ -1,1 +1,41 @@
+function updateTime() {
+  //Dublin
+  let dublinElement = document.querySelector("#dublin");
+  let dublinDateElement = dublinElement.querySelector(".date");
+  let dublinTimeElement = dublinElement.querySelector(".time");
+  let dublinTime = moment().tz("Europe/Dublin");
+  dublinDateElement.innerHTML = dublinTime.format("MMMM Do YYYY");
+  dublinTimeElement.innerHTML = dublinTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
 
+  //Cairo
+  let cairoElement = document.querySelector("#cairo");
+  let cairoDateElement = cairoElement.querySelector(".date");
+  let cairoTimeElement = cairoElement.querySelector(".time");
+  let cairoTime = moment().tz("Africa/Cairo");
+  cairoDateElement.innerHTML = cairoTime.format("MMMM Do YYYY");
+  cairoTimeElement.innerHTML = cairoTime.format("h:mm:ss [<small>]A[</small>]");
+
+  //Tokyo
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
+
+  //London
+
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonTime = moment().tz("Europe/London");
+  londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+  londonTimeElement.innerHTML = londonTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
