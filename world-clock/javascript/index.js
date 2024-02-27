2d6c4743cbc1a6ee9eaab22579a6e24fb3ed3036
ilobna <lobna.elkerdawy@yahoo.com>
2024-02-27 15:18:15 +0000

    Added the current location

--- world-clock/javascript/index.js
+++ world-clock/javascript/index.js
@@ -21,6 +21,9 @@ function updateTime() {
 //Select Element
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
